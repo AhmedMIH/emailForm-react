3f88f64f33b7edda6f5071da7451dded78bb1500
AhmedMIH <m_rx_22@yahoo.com>
2021-05-08 03:33:37 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { send } from 'emailjs-com';
 
 function App() {
+
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_o3yfxr4',
+      'template_jo3nfcf',
+      toSend,
+      'user_r2gGB928jnf1kACv1yXzL'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          name='from_name'
+          placeholder='from name'
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='to_name'
+          placeholder='to name'
+          value={toSend.to_name}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='message'
+          placeholder='Your message'
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          name='reply_to'
+          placeholder='Your email'
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <button type='submit' >Submit</button>
+      </form>
     </div>
   );
 }
-
 export default App;
